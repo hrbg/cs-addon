0334cd3e2de186ae6b79dd2bd8f7975e0dd9b944
Hector Bavio <hectorbavio@gmail.com>
2012-04-04 11:22:23 -0300

    Add domain keys and domain password vars

--- app/controllers/cloudsponge_controller.rb
+++ app/controllers/cloudsponge_controller.rb
@@ -5,6 +5,8 @@ class CloudspongeController < ApplicationController
 
   # provision
   def create
-    render json: {hello: "world"}, status: 200
+    render json: {id: "cloudsponge_id", 
+      config: {CLOUDSPONGE_DOMAIN_KEY: "domain_key", CLOUDSPONGE_DOMAIN_PASSWORD: "domain_password"}}, 
+    status: 200
   end
 end
